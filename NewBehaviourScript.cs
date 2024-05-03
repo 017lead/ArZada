60d1ff7902516f8db96d4f691fa57675bd7fbe26
017lead <141877618+017lead@users.noreply.github.com>
2024-05-02 21:06:58 -0500

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class GyroCameraController : MonoBehaviour
+{
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+
+    private GameObject cameraContainer;
+    private Quaternion rot;
+
+    private void Start()
+    {
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+
+        gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f); // Adjust rotation if needed
+            rot = new Quaternion(0, 0, 1, 0);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Update()
+    {
+        if (gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rot;
+        }
+    }
+}
